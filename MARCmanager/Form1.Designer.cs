6d9bfb0671d3188c779bac634ff4bca8d3ae20ec
Magnus Olofsson <magnusolofsson@outlook.com>
2021-05-07 18:38:12 +0200

    Add project files.

---
+++ MARCmanager/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿
+namespace MARCmanager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importMARCFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportToXLSXFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.chkUsePlacementDataFrom = new System.Windows.Forms.CheckBox();
+            this.cbPlacements = new System.Windows.Forms.ComboBox();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.listViewMain = new System.Windows.Forms.ListView();
+            this.columnAuthor = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnTitle = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnType = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnPlacement = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "MARC Files|*.iso2709|All files|*.*";
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.DefaultExt = "xlsx";
+            this.saveFileDialog.Filter = "Excel Files|*.xlsx|All files|*.*";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(827, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.importMARCFileToolStripMenuItem,
+            this.exportToXLSXFileToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // importMARCFileToolStripMenuItem
+            // 
+            this.importMARCFileToolStripMenuItem.Name = "importMARCFileToolStripMenuItem";
+            this.importMARCFileToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.importMARCFileToolStripMenuItem.Text = "Import MARC file...";
+            this.importMARCFileToolStripMenuItem.ToolTipText = "Click to import a MARC File";
+            this.importMARCFileToolStripMenuItem.Click += new System.EventHandler(this.importMARCFileToolStripMenuItem_Click);
+            // 
+            // exportToXLSXFileToolStripMenuItem
+            // 
+            this.exportToXLSXFileToolStripMenuItem.Enabled = false;
+            this.exportToXLSXFileToolStripMenuItem.Name = "exportToXLSXFileToolStripMenuItem";
+            this.exportToXLSXFileToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exportToXLSXFileToolStripMenuItem.Text = "Export to XLSX file...";
+            this.exportToXLSXFileToolStripMenuItem.ToolTipText = "Click to export the list to an Excel file.";
+            this.exportToXLSXFileToolStripMenuItem.Click += new System.EventHandler(this.exportToXLSXFileToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.ToolTipText = "Exits the application";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // chkUsePlacementDataFrom
+            // 
+            this.chkUsePlacementDataFrom.AutoSize = true;
+            this.chkUsePlacementDataFrom.Location = new System.Drawing.Point(12, 23);
+            this.chkUsePlacementDataFrom.Name = "chkUsePlacementDataFrom";
+            this.chkUsePlacementDataFrom.Size = new System.Drawing.Size(148, 17);
+            this.chkUsePlacementDataFrom.TabIndex = 5;
+            this.chkUsePlacementDataFrom.Text = "Use Placement data from:";
+            this.chkUsePlacementDataFrom.UseVisualStyleBackColor = true;
+            this.chkUsePlacementDataFrom.CheckedChanged += new System.EventHandler(this.chkUsePlacementDataFrom_CheckedChanged);
+            // 
+            // cbPlacements
+            // 
+            this.cbPlacements.Enabled = false;
+            this.cbPlacements.FormattingEnabled = true;
+            this.cbPlacements.Location = new System.Drawing.Point(166, 21);
+            this.cbPlacements.Name = "cbPlacements";
+            this.cbPlacements.Size = new System.Drawing.Size(185, 21);
+            this.cbPlacements.TabIndex = 6;
+            this.cbPlacements.SelectedIndexChanged += new System.EventHandler(this.cbPlacements_SelectedIndexChanged);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusLabel});
+            this.statusStrip.Location = new System.Drawing.Point(0, 743);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.ShowItemToolTips = true;
+            this.statusStrip.Size = new System.Drawing.Size(827, 22);
+            this.statusStrip.SizingGrip = false;
+            this.statusStrip.TabIndex = 7;
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(0, 17);
+            // 
+            // listViewMain
+            // 
+            this.listViewMain.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnAuthor,
+            this.columnTitle,
+            this.columnType,
+            this.columnPlacement});
+            this.listViewMain.FullRowSelect = true;
+            this.listViewMain.GridLines = true;
+            this.listViewMain.HideSelection = false;
+            this.listViewMain.Location = new System.Drawing.Point(0, 3);
+            this.listViewMain.Name = "listViewMain";
+            this.listViewMain.Size = new System.Drawing.Size(827, 689);
+            this.listViewMain.TabIndex = 2;
+            this.listViewMain.UseCompatibleStateImageBehavior = false;
+            this.listViewMain.View = System.Windows.Forms.View.Details;
+            // 
+            // columnAuthor
+            // 
+            this.columnAuthor.Text = "Author";
+            this.columnAuthor.Width = 200;
+            // 
+            // columnTitle
+            // 
+            this.columnTitle.Text = "Title";
+            this.columnTitle.Width = 200;
+            // 
+            // columnType
+            // 
+            this.columnType.Text = "Type";
+            this.columnType.Width = 150;
+            // 
+            // columnPlacement
+            // 
+            this.columnPlacement.Text = "Placement";
+            this.columnPlacement.Width = 150;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.listViewMain);
+            this.panel1.Location = new System.Drawing.Point(0, 48);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(836, 692);
+            this.panel1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(827, 765);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.cbPlacements);
+            this.Controls.Add(this.chkUsePlacementDataFrom);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "MARCmanager";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem importMARCFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportToXLSXFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.CheckBox chkUsePlacementDataFrom;
+        private System.Windows.Forms.ComboBox cbPlacements;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel statusLabel;
+        private System.Windows.Forms.ListView listViewMain;
+        private System.Windows.Forms.ColumnHeader columnAuthor;
+        private System.Windows.Forms.ColumnHeader columnTitle;
+        private System.Windows.Forms.ColumnHeader columnType;
+        private System.Windows.Forms.ColumnHeader columnPlacement;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ MARCmanager/Form1.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MARC;
+using SwiftExcel;
+
+namespace MARCmanager
+{
+    public partial class Form1 : Form
+    {
+        #region methods and stuff
+        private FileMARC myMARC;
+        private List<string> permanentLocations = new List<string>();
+        private int marcRecordCount;
+        
+
+        private void importMARCFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fromPath = openFileDialog.FileName;
+                myMARC = new FileMARC();
+                myMARC.ImportMARC(fromPath);
+                permanentLocations.Clear();
+                cbPlacements.Items.Clear();
+                cbPlacements.Enabled = false;
+                cbPlacements.Text = "";
+                chkUsePlacementDataFrom.Checked = false;
+                CheckMARCData();
+                UpdateListView("");
+                exportToXLSXFileToolStripMenuItem.Enabled = true;
+            }
+        }
+
+        private void CheckMARCData()
+        {
+            marcRecordCount = myMARC.Count;
+            statusLabel.Text = $"Loaded {marcRecordCount} Records.";
+            foreach (Record record in myMARC)
+            {
+                List<Field> locationFields = record.GetFields("952");
+                if (locationFields != null && locationFields[0].IsDataField())
+                {
+                    foreach (DataField locationDataField in locationFields)
+                    {
+                        Subfield libraryData = locationDataField['a'];
+                        if (!permanentLocations.Contains(libraryData.Data))
+                        {
+                            permanentLocations.Add(libraryData.Data);
+                            Console.WriteLine(libraryData.Data);
+                        }
+                    }
+                }
+            }
+            myMARC.Reset();
+
+        }
+
+        private void ReloadPlacement()
+        {
+            cbPlacements.Items.Clear();
+            foreach (var loc in permanentLocations)
+            {
+                cbPlacements.Items.Add(loc);
+            }
+            cbPlacements.Enabled = true;
+        }
+
+        private void InitListView(bool showPlacement=false)
+        {
+            listViewMain.Items.Clear();
+            listViewMain.Columns.Clear();
+            listViewMain.Columns.Add("author", "Author", 200);
+            listViewMain.Columns.Add("title", "Title", 200);
+            listViewMain.Columns.Add("type", "Type", 150);
+            if (showPlacement)
+            {
+                listViewMain.Columns.Add("placement", "Placement", 150);
+            }
+        }
+
+        private void UpdateListView(string placementDataFilter)
+        {
+
+            InitListView(placementDataFilter == "" ? false : true);
+            foreach (Record record in myMARC)
+            {
+                Field authorField = record["100"];
+                Field titleField = record["245"];
+                Field typeField = record["942"];
+                List<Field> locationFields = record.GetFields("952");
+
+                string Author;
+                string CoAuthors = "";
+                string Title;
+                string Type;
+                string Placement;
+
+                #region authorfield
+                if (authorField == null)
+                {
+                    Author = "";
+                }
+                else
+                {
+                    if (authorField.IsDataField())
+                    {
+                        DataField authorDataField = (DataField)authorField;
+                        Subfield authorName = authorDataField['a'];
+                        Author = authorName.Data;
+                    }
+                    else
+                    {
+                        Author = "";
+                    }
+                }
+                #endregion
+
+                #region titleField
+                Title = "";
+                if (titleField == null)
+                {
+                    Title = "";
+                }
+                else
+                {
+                    if (titleField.IsDataField())
+                    {
+                        DataField titleDataField = (DataField)titleField;
+                        Subfield titleA = titleDataField['a'];
+                        Subfield titleB = titleDataField['b'];
+                        Subfield coAuthorsField = titleDataField['c'];
+
+                        if (titleA != null)
+                        {
+                            Title += titleA.Data;
+                            if (Title.EndsWith("/"))
+                            {
+                                Title = Title.Substring(0, Title.Length - 1);
+                            }
+                        }
+                        if (titleB != null)
+                        {
+                            Title = Title.TrimEnd();
+                            Title += " ";
+                            Title += titleB.Data;
+                            if (Title.EndsWith("/"))
+                            {
+                                Title = Title.Substring(0, Title.Length - 1);
+                            }
+                        }
+                        if (coAuthorsField != null)
+                        {
+                            CoAuthors = coAuthorsField.Data;
+                        }
+                    }
+                    else
+                    {
+                        Title = "";
+                    }
+                }
+                if (Title.EndsWith("."))
+                {
+                    Title = Title.Substring(0, Title.Length - 1);
+                }
+                #endregion
+
+                #region typeField
+                Type = "";
+                if (typeField != null)
+                {
+                    if (typeField.IsDataField())
+                    {
+                        DataField typeDataField = (DataField)typeField;
+                        Subfield typeData = typeDataField['c'];
+                        if (typeData != null)
+                        {
+                            Type = typeData.Data;
+                        }
+
+                    }
+                    else
+                    {
+                        Type = "";
+                    }
+                }
+                #endregion
+
+                #region locationField
+                Placement = "";
+                if (locationFields != null)
+                {
+                    if (locationFields[0].IsDataField())
+                    {
+                        if (placementDataFilter != null && placementDataFilter !="")
+                        {
+                            if (Placement == "")
+                            {
+                                foreach (DataField locationDataField in locationFields)
+                                {
+                                    Subfield libraryData = locationDataField['a'];
+                                    Subfield placementData = locationDataField['c'];
+                                    if (libraryData.Data == placementDataFilter)
+                                    {
+                                        if(placementData!=null)
+                                        {
+                                            Placement = placementData.Data == null ? "" : placementData.Data;
+                                        }
+                                        else
+                                        {
+                                            Placement = "";
+                                        }
+                                        
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Placement = "";
+                    }
+                }
+                #endregion
+
+
+                if (Author.EndsWith(","))
+                {
+                    Author = Author.Substring(0, Author.Length - 1);
+                }
+
+                ListViewItem lvi = new ListViewItem(Author);
+                lvi.SubItems.Add(Title);
+                lvi.SubItems.Add(Type);
+                if (placementDataFilter != null)
+                {
+                    lvi.SubItems.Add(Placement);
+                }
+                listViewMain.Items.Add(lvi);
+            }
+            myMARC.Reset();
+            if(placementDataFilter!="")
+            {
+                statusLabel.Text = $"Fetched placement data for all items located in {placementDataFilter} (if they have a placement there...)";
+            }
+        }
+
+
+        private void exportFile()
+        {
+            if(saveFileDialog.ShowDialog()==DialogResult.OK)
+            {
+                string savePath = saveFileDialog.FileName;
+                WriteExcel(savePath);
+                statusLabel.Text = $"Exported {marcRecordCount} Records to {savePath}";
+            }
+        }
+
+        private void WriteExcel(string savePath)
+        {
+            using (var ew = new ExcelWriter(savePath))
+            {
+                int columnCounter = 1;
+                int xlRow = 1;
+                foreach(ColumnHeader col in listViewMain.Columns)
+                {
+                    ew.Write(col.Text, columnCounter, xlRow);
+                    columnCounter++;
+                }
+                
+                xlRow++;
+                
+                foreach(ListViewItem item in listViewMain.Items)
+                {
+                    columnCounter = 0;
+                    //ew.Write(item.Text, columnCounter, xlRow);
+                    foreach(ListViewItem.ListViewSubItem subitem in item.SubItems)
+                    {
+                        columnCounter++;
+                        ew.Write(subitem.Text, columnCounter, xlRow);
+                    }
+                    xlRow++;
+                }
+            }
+        }
+
+        #endregion
+
+        #region formstuff
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+     
+        private void chkUsePlacementDataFrom_CheckedChanged(object sender, EventArgs e)
+        {
+            if(chkUsePlacementDataFrom.Checked)
+            {
+                ReloadPlacement();
+            }
+            else
+            {
+                cbPlacements.Enabled = false;
+            }
+
+
+        }
+
+        
+        private void cbPlacements_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedPlacement = cbPlacements.SelectedItem.ToString();
+            UpdateListView(selectedPlacement);
+        }
+
+        #endregion
+
+        private void exportToXLSXFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            exportFile();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
